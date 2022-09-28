c2878ca1f2af5e605d62ee9a835f6b665abcf23d
Tiago Valente <tiagoandrevalente@gmail.com>
2022-09-28 23:47:18 +0100

    Day 29 to 31 challenge.

---
+++ Day 29-31/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Tiago Valente on 30/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
